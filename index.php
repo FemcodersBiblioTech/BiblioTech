c728dc5f990cf6783ef1584a75ce764f77fc324b
Sylvia Llorente <sylviall81@gmail.com>
2023-07-26 11:24:26 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+require_once('/Applications/MAMP/htdocs/Bibliotech/view/head/head.php');
+
+?>
+
+
+<a href="/Bibliotech/view/bookRegister/create.php" class = 'btn btn-primary'>Add New Book</a>
+<h1>aqui hay algo</h1>
+
+<?php 
+require_once('/Applications/MAMP/htdocs/Bibliotech/view/head/footer.php');
+
+?>
