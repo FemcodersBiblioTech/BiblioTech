2d3ea6e82cb011d4a5fef96bdfff3f449c566a2d
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-07-27 22:32:37 +0200

    changes footer

--- View/Footer.php
+++ View/Footer.php
@@ -20,7 +20,7 @@
                             class='logo-icons' />Twitter
                     </li>
                     <li class='flex flex-col items-center xl:flex-row'><img src='./assets/Facebook.svg'
-                            alt='facebook icon' className=' h-[1.2em] w-[1.2em] mb-[0.15em] xl:mr-3 xl:mb-0' />Facebook
+                            alt='facebook icon' class=' h-[1.2em] w-[1.2em] mb-[0.15em] xl:mr-3 xl:mb-0' />Facebook
                     </li>
                 </ul>
             </div>
