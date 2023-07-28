b971b2553ddb3e041b90e5472cd6df763c4bac2b
ZoraidaMorenoCadenas <zoraidamorenocadenas79@gmail.com>
2023-07-28 11:06:28 +0200

    styles changes

--- View/Footer.php
+++ View/Footer.php
@@ -2,15 +2,13 @@
     <div class='box'>
         <div class='flexbox'>
             <ul class='flex'>
-                <li class='margin-li'>About us</li>
+                <li>About us</li>
                 <li>Contact</li>
             </ul>
             <ul class='flex'>
-                <li class='flex margin-li'><img src='./assets/Twitter.svg' alt='twitter icon'
-                        class='logo-icons' />Twitter
+                <li class='flex margin-li'>Twitter
                 </li>
-                <li class='flex flex-col items-center xl:flex-row'><img src='./assets/Facebook.svg' alt='facebook icon'
-                        class=' h-[1.2em] w-[1.2em] mb-[0.15em] xl:mr-3 xl:mb-0' />Facebook
+                <li class='flex'>Facebook
                 </li>
             </ul>
         </div>
